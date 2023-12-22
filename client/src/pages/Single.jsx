1d0dbd273b7a5390bf95be84e7ad66da368cb28c
shokeensanjana <sashokeen@expediagroup.com>
2023-12-22 17:41:10 +0530

    Build SinglePost and Write Page.

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Edit from "../img/edit.png"
 import Delete from "../img/delete.png"
+import User from "../img/user.png"
+import Menu from "../components/Menu"
 import {Link} from 'react-router-dom'
 
  
@@ -10,7 +12,7 @@ const Single = () =>{
             <div className ="content">
             <img src ="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt =""></img>
             <div className="user">
-            <img src ="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2 " alt =""></img>
+            <img src ={User} alt =""></img>
         <div className="info">
         <span> Sanjana</span>
         <p>Posted 2 days ago </p>
@@ -22,8 +24,12 @@ const Single = () =>{
         <img src={Delete} alt=""/>
         </div>
      </div>
+     <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit"</h1>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+    <br></br>
+    <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
      </div>
-            <div className="menu">Menu</div>
+        <Menu/>
       </div>
     )
 }
--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'
 
-const Write = () =>{
+const Write = () =>{ 
+    const [value, setValue] = useState('');
+    console.log(value)
     return (
-        <div>Write</div>
+        <div className="add">
+            <div className="content">
+                <input type="text" placeholder="Title"></input>
+            <div className="editorContainer">
+            <ReactQuill theme="snow" value={value} onChange={setValue} />
+            </div>
+            
+            </div>
+            <div className="menu">
+            <div className="item">
+                <h1>Publish</h1>
+                <span>
+                    <b>Status:</b> Draft
+                </span>
+                <span>
+                    <b>Visibility:</b> Public
+                </span>
+                <input style= {{display:"none"}} type="file" name="" id="file" />
+                <label className="file" htmlFor="file">Uplaod</label>
+                <div className="buttons">
+                    <button>Save as Draft</button>
+                    <button>Update</button>
+                </div>
+
+                
+                </div>
+            <div className="item">
+                <h1>Category</h1>
+                <div className="cat">
+                <input type="radio" name="cat" value="art"  id="art"/>
+                <label htmlFor="art">ART</label>
+                </div>
+                <div className="cat">
+                <input type="radio" name="cat" value="science"  id="science"/>
+                <label htmlFor="science">SCIENCE</label>
+                </div>
+                <div className="cat">
+                <input type="radio" name="cat" value="technology"  id="technology"/>
+                <label htmlFor="technology">TECHNOLOGY</label>
+                </div>
+                <div className="cat">
+                <input type="radio" name="cat" value="cinema"  id="cinema"/>
+                <label htmlFor="cinema">CINEMA</label>
+                </div>
+                <div className="cat">
+                <input type="radio" name="cat" value="design"  id="design"/>
+                <label htmlFor="design">DESIGN</label>
+                </div>
+                <div className="cat">
+                <input type="radio" name="cat" value="food"  id="food"/>
+                <label htmlFor="food">FOOD</label>
+                </div>
+            </div>
+            </div>
+
+        </div>
     )
 }
 
